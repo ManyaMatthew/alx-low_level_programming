0dc7148b7eb51411fce2b4be037e5a48bbea2ed3
ManyaMatthew <manyamatt33@gmail.com>
2022-12-11 19:36:22 -0800

    YES

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
-#include <unistd.h>
-
 /**
- * main - prints exactly and that piece of art is useful
- * 
- * Return: 1
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
  */
 
 int main(void)
-
 {
+char str1[] = "and that piece of art is useful\" - Dora korpar, 2015-10-19)\n";
 
-         write(2 "and that piece of art is useful\" - Dora korpar, 2015-10-19);
-         return (1);
+write(2, str1, 59);
+return (1);
 }
 
